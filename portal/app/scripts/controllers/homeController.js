3b1fefde9d8e2d561b309d3c12dab6e433670d88
xiaomx <xiaomixin@gmail.com>
2016-03-23 00:14:42 +0800

    [Xiao] add products

--- portal/app/scripts/controllers/homeController.js
+++ portal/app/scripts/controllers/homeController.js
@@ -11,4 +11,25 @@ angular.module('portalApp')
     .controller('HomeCtrl', function ($scope, messageCN) {
         $scope.navItems = messageCN.nav_items;
         $scope.navIcons = messageCN.nav_icons;
+
+        $scope.products = [
+            {
+                name: "operator",
+                title: "My Title",
+                desc: "This area is 100% editable and can be used to promote your products and services.  " +
+                    "This area is 100% editable and can be used to promote your products and services."
+            },
+            {
+                name: "software",
+                title: "My Title",
+                desc: "This area is 100% editable and can be used to promote your products and services.  " +
+                    "This area is 100% editable and can be used to promote your products and services."
+            },
+            {
+                name: "calculator",
+                title: "My Title",
+                desc: "This area is 100% editable and can be used to promote your products and services.  " +
+                    "This area is 100% editable and can be used to promote your products and services."
+            }
+        ]
     });
