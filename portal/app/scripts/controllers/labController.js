bf1f02e1832c5b6596f474f67f2e48d74c86642e
xiaomx <xiaomixin@gmail.com>
2016-03-29 23:38:38 +0800

    [Xiao] add lab tab & cloud watch sub tab

--- portal/app/scripts/controllers/labController.js
+++ portal/app/scripts/controllers/labController.js
@@ -10,10 +10,17 @@
 angular.module('portalApp')
     .controller('LabCtrl', function ($scope, messageCN) {
     })
-    .controller('CloudWatchCtrl', function ($scope, messageCN) {
+    .controller('CloudWatchCtrl', function ($scope, $http, CommonService) {
+        const CLOUD_WATCH_REST_URL = "cloud/cloudwatch/";
         $scope.alarms = [
-            {
-
-            }
+            {}
         ];
+
+        CommonService.get(GLOBAL_ENV.urlPrefix + CLOUD_WATCH_REST_URL + "alarms")
+            .then(function (content) {
+                console.log(content);
+                if (!content && !content.length) {
+                    $scope.alarms = content;
+                }
+            });
     });
