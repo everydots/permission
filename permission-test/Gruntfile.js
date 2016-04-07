e127e136d7e5e4f55bf90eecf48e9af12763caa1
xiaomx <xiaomixin@gmail.com>
2016-04-07 17:21:53 +0800

    add uglify

--- permission-test/Gruntfile.js
+++ permission-test/Gruntfile.js
@@ -3,15 +3,6 @@ module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
-            }
-        },
         jshint: {
             files: ['Gruntfile.js', 'specs/*.js'],
             options: {
@@ -21,6 +12,32 @@ module.exports = function (grunt) {
                     console: true,
                     module: true,
                     document: true
+                }
+            }
+        },
+        useminPrepare: {
+            html: 'app/index.html',
+            options: {
+                dest: 'app/dist',
+                flow: {
+                    html: {
+                        steps: {
+                            js: ['concat', 'uglify']
+                        },
+                        post: {}
+                    }
+                }
+            }
+        },
+        usemin: {
+            html: ['app/index.html'],
+            js: ['app/dist/{,*/}*.js'],
+            options: {
+                blockReplacements: {
+                    js: function (block) {
+                        console.log(block);
+                        return '<script type="application/javascript" src="dist/' + block.dest + '"></script>'
+                    }
                 }
             }
         },
@@ -49,10 +66,13 @@ module.exports = function (grunt) {
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-protractor-runner');
+    grunt.loadNpmTasks('grunt-usemin');
 
     // Default task(s).
     grunt.registerTask('default', ['uglify', 'jshint']);
     grunt.registerTask('e2e', ['protractor:e2e']);
+    grunt.registerTask('minify', ['useminPrepare', 'concat:generated', 'uglify:generated']);
 
 };
