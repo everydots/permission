b349f659fa16144493faa57267ce39538144b385
xiaomx <xiaomixin@gmail.com>
2016-04-10 17:46:45 +0800

    [Star] add articles panel

---
+++ portal/app/scripts/controllers/articlesController.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name portalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the portalApp
+ */
+angular.module('portalApp')
+  .controller('ArticlesCtrl', function ($scope, messageCN) {
+
+  });
