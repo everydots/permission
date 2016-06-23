591fb851bb00f9f2a6d2b944b71b4b471c4c8ae5
xiaomx <xiaomixin@gmail.com>
2016-06-23 17:37:21 +0800

    [Xiao] finish view part of project

--- cost-web-ui/app/scripts/controllers/cost-analysis.js
+++ cost-web-ui/app/scripts/controllers/cost-analysis.js
@@ -8,21 +8,23 @@
  * Controller of the stockWebUiApp
  */
 angular.module('costAnalysisApp')
-    .controller('CostAnalysisCtrl', function ($scope, $http, Constants) {
-        $scope.labels = [];
-        $scope.data = [];
-        $scope.series = ["Service Cost"];
+  .controller('CostAnalysisCtrl', function ($scope, $http, Constants) {
+    $scope.labels = [];
+    $scope.data = [];
+    $scope.series = ["Service Cost"];
 
-        $http
-            .get(Constants.service_urls.service_cost_statistic)
-            .then(function (content) {
-                if (content.status == 200 && angular.isArray(content.data)) {
-                    content.data.forEach(function (item) {
-                        $scope.labels.push(item.service_name);
-                        $scope.data.push(item.cost);
-                    });
-                }
-            });
+    $http
+      .get(Constants.service_urls.service_cost_statistic)
+      .then(function (content) {
+        if (content.status == 200 && angular.isArray(content.data)) {
+          var dataArray = [];
+          content.data.forEach(function (item) {
+            $scope.labels.push(item.service_name);
+            dataArray.push(item.cost);
+          });
+          $scope.data.push(dataArray);
+        }
+      });
 
 
-    });
+  });
