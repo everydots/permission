09bb94a2fabfb485a738fb786ba3142c9fa35147
SiyuYan <18200289607@163.com>
2016-06-25 22:40:47 +0800

    [Siyu]destroy IAM

--- cloud-base/src/main/java/com/everydots/cloud/aws/IAMClient.java
+++ cloud-base/src/main/java/com/everydots/cloud/aws/IAMClient.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.identitymanagement.model.*;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
 
@@ -13,10 +14,13 @@ public class IAMClient {
     public static final String ACCESSKEY = "accessKey";
     public static final String SECRETKEY = "secretKey";
     public static final String PROPERTIES = "src/main/resources/awsCredentials.properties";
+    public static final String CREDENTIAL_PROFILE = "awsCredentials.properties";
+
     public static AmazonIdentityManagementClient instance = null;
+    Properties prop = new Properties();
 
     public Properties writeCredentials() throws IOException {
-        Properties prop = new Properties();
+
         OutputStream output = new FileOutputStream(PROPERTIES);
 
         AccessKey IAM = createIAM();
@@ -48,8 +52,10 @@ public class IAMClient {
         return createUserResult.getUser().getUserName();
     }
 
-    public void destroyIAM() {
-        DeleteAccessKeyRequest deleteAccessKeyRequest = new DeleteAccessKeyRequest().withUserName(IAMUSER);
+    public void destroyIAM() throws IOException {
+        InputStream inputStream=getClass().getClassLoader().getResourceAsStream(CREDENTIAL_PROFILE);
+        prop.load(inputStream);
+        DeleteAccessKeyRequest deleteAccessKeyRequest = new DeleteAccessKeyRequest().withUserName(IAMUSER).withAccessKeyId(prop.getProperty(ACCESSKEY));
         DeleteUserRequest deleteUserRequest = new DeleteUserRequest().withUserName(IAMUSER);
         getIAMClient().deleteAccessKey(deleteAccessKeyRequest);
         getIAMClient().deleteUser(deleteUserRequest);
